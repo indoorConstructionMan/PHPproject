9bd59e0d6d2b156f28107dc0853c1f17298bc94c
machinemessiah <mark.edward.franklin@gmail.com>
2016-06-30 15:29:13 -0600

    Tweaked the style of the login and register views some more.

--- View/Index/login.php
+++ View/Index/login.php
@@ -4,25 +4,25 @@
             <div class="row">
                 <div class="col s12">
                     <div class="center promo promo-example">
-                      <i class="material-icons greensea emerald-text icon">lock_open</i>
+                      <i class="material-icons moss emerald-text icon">lock_open</i>
                     </div>
                 </div>
             </div>
             <div class="row"> 
                 <div class="input-field col s12">
-                    <input class="clouds-text greensea-border bold" id="email" type="text" name="email" required>
-                    <label for="email" class="greensea-text">email address or username</label>
+                    <input class="clouds-text moss-border bold" id="email" type="text" name="email" required>
+                    <label for="email" class="moss-text">email address or username</label>
                 </div>
             </div>
             <div class="row"> 
                 <div class="input-field col s12">
-                    <input class="clouds-text greensea-border bold" id="password" type="password" name="password" required>
-                    <label for="password" class="greensea-text">password</label>
+                    <input class="clouds-text moss-border bold" id="password" type="password" name="password" required>
+                    <label for="password" class="moss-text">password</label>
                 </div>
             </div>
             <div class="row">
-                <button type="submit" class="col s12 greensea-text bold" style="">login</button>
-                <a href="register" class="form-link greensea-text" style="">or click here to create an account</a>
+                <button type="submit" class="col s12 moss-text bold" style="">login</button>
+                <a href="register" class="form-link moss-text" style="">or click here to create an account</a>
             </div>
         </form>
     </div>
--- View/Index/register.php
+++ View/Index/register.php
@@ -4,37 +4,37 @@
             <div class="row">
                 <div class="col s12">
                     <div class="center promo promo-example">
-                      <i class="material-icons greensea emerald-text icon">person_pin</i>
+                      <i class="material-icons moss emerald-text icon">person_pin</i>
                     </div>
                 </div>
             </div>
             <div class="row"> 
                 <div class="input-field col s12">
-                    <input class="clouds-text greensea-border bold" id="email" type="email" name="email" required>
-                    <label for="email" class="greensea-text">email address</label>
+                    <input class="clouds-text moss-border bold" id="email" type="email" name="email" required>
+                    <label for="email" class="moss-text">email address</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s12">
-                    <input class="clouds-text greensea-border bold" id="username" type="text" name="username" required>
-                    <label for="username" class="greensea-text">username</label>
+                    <input class="clouds-text moss-border bold" id="username" type="text" name="username" required>
+                    <label for="username" class="moss-text">username</label>
                 </div> 
             </div>
             <div class="row"> 
                 <div class="input-field col s12">
-                    <input class="clouds-text greensea-border bold" id="password" type="password" name="password" required>
-                    <label for="password" class="greensea-text">password</label>
+                    <input class="clouds-text moss-border bold" id="password" type="password" name="password" required>
+                    <label for="password" class="moss-text">password</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s12">
-                    <input class="clouds-text greensea-border bold" id="password_confirm" type="password" name="password_confirm" required>
-                    <label for="password_confirm" class="greensea-text">confirm password</label>
+                    <input class="clouds-text moss-border bold" id="password_confirm" type="password" name="password_confirm" required>
+                    <label for="password_confirm" class="moss-text">confirm password</label>
                 </div> 
             </div>
             <div class="row">
-                <button type="submit" class="col s12 greensea-text bold" style="">create account</button>
-                <a href="login" class="form-link greensea-text" style="">or click here to login</a>
+                <button type="submit" class="col s12 moss-text bold" style="">create account</button>
+                <a href="login" class="form-link moss-text" style="">or click here to login</a>
             </div>
         </form>
     </div>
