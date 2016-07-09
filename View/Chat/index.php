38ac2dea2ffa4bcb4877c08bd595e385a4a5e4c8
Aaron <indoorConstructionMan@gmail.com>
2016-07-08 22:53:26 -0600

    Just added a navbar, Edit profile has nothing. Logout works properly. The dropdown menu needs to be more clickable. No functionality implemented.

--- View/Chat/index.php
+++ View/Chat/index.php
@@ -1,13 +1,27 @@
-<h1>welcome</h1>
-<h3><?=$_SESSION['chatapp_user']->username?></h3>
-<p>have fun chatting on our chat app!</p>
-<a href="logout">LOGOUT</a>
+<nav class="nav-wrapper red accent-2 slidein-top">
+    
+    
+    <a href="#" class="flow-text left">Welcome  <?= $_SESSION['chatapp_user']->username ?>!</a>
+    
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <!-- Dropdown Trigger -->
+        <li><a class='dropdown-button emerald btn white-text' href='#' data-activates='dropdown1'>Settings</a></li>
 
-<form method="post" action="content" autocomplete="off" class="slidein-top-components">
-    <div class="row">
-        <div class="input-field col s12">
-            <input class="clouds-text moss-border bold flow-text" autocomplete="off" id="content" type="content" name="content" >
-            <label for="content" class="moss-text flow-text">Click.   Type.    Press Enter.</label>
-        </div>
-    </div>
-</form>
+        <!-- Dropdown Structure -->
+        <ul id='dropdown1' class='dropdown-content emerald-text'>
+            <li><a href="#!">Edit Profile</a></li>
+            <li class="divider"></li>
+            <li><a href="logout">Logout</a></li>
+        </ul>
+    </ul>
+    
+    
+    
+    <!---<a href="#"><i class="material-icons center-align">search</i></a>--->
+</nav>
+
+<div class="input-field col s12">
+    <i class="material-icons prefix">mode_edit</i>
+    <textarea id="textarea1" class="materialize-textarea belize-text moss-border bold flow-text"></textarea>
+    <label class="moss-text flow-text" for="textarea1">Click. Type. Press enter.</label>
+</div>
