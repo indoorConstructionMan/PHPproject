4698d52f644eef238cba1d9b21f05c2050a4a887
Aaron <indoorConstructionMan@gmail.com>
2016-07-10 00:52:36 -0600

    Search bar is now fully implemented. In the sense that if you type the exact username or email than you will be returned a table. If there are no matches, it will give an error message. This information reflects is_online status as well. Currently, the chat button routes to logout.

--- View/Chat/_search.php
+++ View/Chat/_search.php
@@ -1,10 +1,40 @@
-<? if (isset($_SESSION['view_vars']->data->username)) :  ?>
+<? if (isset($_SESSION['view_vars']->data->username)) : ?>
 
     <div class="row">
         <div class="col s12">
             <div class="center promo promo-example">
-                <p><?= $_SESSION['view_vars']->data->username ?></p>
-                <p><?= $_SESSION['view_vars']->data->is_online ?></p>
+                <div class="col s4 offset-s4 padding5 row">
+                    <div class="col s12">
+                        <div class="center promo promo-example">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th style="text-align: center;" data-field="username">username</th>
+                                        <th style="text-align: center;" data-field="is_online">is online?</th>
+                                        <th style="text-align: center;" data-field="begin_chat">begin chat</th>
+                                    </tr>
+                                </thead>
+                                <? if ($_SESSION['view_vars']->data->is_online) : ?>
+                                    <tbody>
+                                        <tr>
+                                            <td style="text-align: center;"><?= $_SESSION['view_vars']->data->username ?></td>
+                                            <td style="text-align: center;"><i class="s4 small material-icons moss emerald-text icon">thumb_up</i></td>
+                                            <td style="text-align: center;"><a href="logout"><i class="s4 small material-icons moss emerald-text icon">chat</i></a></td>
+                                        </tr>
+                                    </tbody>
+                                <? else: ?>
+                                    <tbody>
+                                        <tr>
+                                            <td style="text-align: center;"><?= $_SESSION['view_vars']->data->username ?></td>
+                                            <td style="text-align: center;"><i class="col s4 mall material-icons moss emerald-text icon">thumb_down</i></td>
+                                            <td style="text-align: center;"><a href="logout"><i class="col s4 small material-icons moss emerald-text icon">chat</i></a></td>
+                                        </tr>
+                                    </tbody>
+                                <? endif; ?>
+                            </table> 
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
