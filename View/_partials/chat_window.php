a846d05eeeb9757b1a362e78fb3b12fb71949cb9
machinemessiah <mark.edward.franklin@gmail.com>
2016-07-11 17:51:45 -0600

    WRapped foreach in chat_window partial with a conditional to check if there are messages before trying to loop through them.

--- View/_partials/chat_window.php
+++ View/_partials/chat_window.php
@@ -1,8 +1,10 @@
 <div id="chat_window_<?= $chat_window->id ?>" class="chat_window full-height-100 chat-tab-fix">
     <div class="chat_window_output full-height-80 clouds white-border border15 padding1">
-        <?foreach ($chat_window->messages as $message) :?>
-            <?require("/View/_partials/chat_message.php");?>
-        <?endforeach;?>
+        <?if (is_array($chat_window->messages) && !empty($chat_window->messages)) :?>
+            <?foreach ($chat_window->messages as $message) :?>
+                <?require("/View/_partials/chat_message.php");?>
+            <?endforeach;?>
+        <?endif;?>
     </div>
     <div class="chat_window_input full-height-20">
         <textarea class="full-height-100 full-width moss clouds-border clouds-text large-font-size padding1"></textarea>
