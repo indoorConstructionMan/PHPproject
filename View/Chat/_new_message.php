ead56a6a215785fcf8d413fbae39532a836e1d45
machinemessiah <mark.edward.franklin@gmail.com>
2016-07-11 15:57:40 -0600

    Adding view for _new_message action. Added partial for new message.

---
+++ View/Chat/_new_message.php
@@ -0,0 +1,2 @@
+<?$message = $_SESSION['view_vars']->message;require("/View/_partials/chat_message.php")?>
+
---
+++ View/_partials/chat_message.php
@@ -0,0 +1,4 @@
+<div class='chat_message'>
+    <span class='emerald-text bold'><?=$message->username?></span>: <span class=''><?=$message->message?></span>
+</div>
+
